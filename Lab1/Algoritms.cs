d316557472b97317a13cbac7eb5fe1ce19776b82
Ne1P <64397727+Ne1P@users.noreply.github.com>
2020-10-12 15:10:52 +0300

    Добавьте файлы проекта.

---
+++ Lab1/Algoritms.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab1
+{
+    class Algoritms
+    {
+        public static void CriterionWald(int[,] data)
+        {
+            Console.WriteLine();
+            Console.WriteLine("     Критерій Вальда");
+            int[] temp = new int[3];
+
+            int[] minimums = new int[3];
+            Console.Write("Мінімальні значення з рядка: ");
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                for (int j = 0; j < data.GetLength(1); j++)
+                {
+                    temp[j] = data[i, j];
+                }
+                minimums[i] = temp.Min();
+                Console.Write(minimums[i]+", ");
+            }
+            Console.WriteLine();
+
+            int bestRow = 0;
+
+            for (int i = 0; i < minimums.Count(); i++)
+            {
+                if (minimums.Max() == minimums[i])
+                {
+                    bestRow = i;
+                    Console.WriteLine("Найбільше з цих значень: " + minimums.Max() + "  У рядку: " + (bestRow + 1));
+                }
+            }
+
+            Console.Write("Найкраще рішення: ");
+            for (int j = 0; j < data.GetLength(1); j++)
+            {
+                Console.Write(data[bestRow, j] + ", ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void СriterionLaplace(int[,] data)
+        {
+            Console.WriteLine();
+            
+            Console.WriteLine("     Критерій Лапласа");
+
+            double[] rowSum = new double[3];
+
+
+            Console.Write("Просумовані значення з рядка: ");
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                for (int j = 0; j < data.GetLength(1); j++)
+                {
+                    rowSum[i] += data[i, j];
+                }
+                Console.Write(rowSum[i] + ", ");
+            }
+            Console.WriteLine();
+            Console.Write("Розділені на к-ть рядків значення: ");
+            for (int i = 0; i < rowSum.Count(); i++)
+            {
+                rowSum[i] = Math.Round((rowSum[i] / (data.GetLength(0))), 1);
+                Console.Write(rowSum[i] + ", ");
+            }
+            Console.WriteLine();
+
+            int bestRow = 0;
+
+            for (int i = 0; i < rowSum.Count(); i++)
+            {
+                if (rowSum.Max() == rowSum[i])
+                { 
+                    bestRow = i;
+                    Console.WriteLine("Найбільше з цих значень: "+rowSum.Max()+ "  У рядку: "+(bestRow+1));
+                }
+            }
+
+            Console.Write("Найкраще рішення: ");
+            for (int j = 0; j < data.GetLength(1); j++)
+            {
+                Console.Write(data[bestRow, j] + ", ");
+            }
+            Console.WriteLine();
+
+        }
+
+        public static void CriterionHurwitz(int[,] data)
+        {
+            Console.WriteLine();
+            Console.WriteLine("     Критерій Гурвіца");
+            SubCriterionHurwitz(data, 0.9);
+            SubCriterionHurwitz(data, 0.2);
+        }
+        public static void SubCriterionHurwitz(int[,] data, double coeff)
+        { 
+            int[] maxInRow = new int[3];
+            int[] minInRow = new int[3];
+
+            Console.WriteLine("Коефіцієнт: "+ coeff);
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                minInRow[i] = 101;
+                for (int j = 0; j < data.GetLength(1); j++)
+                {
+                    if (data[i, j] > maxInRow[i])
+                    {
+                        maxInRow[i] = data[i, j];
+                    }
+                    if(data[i, j] < minInRow[i])
+                    {
+                        minInRow[i] = data[i, j];
+                    }
+                }
+               
+            }
+            Console.Write("Максимуми рядків: ");
+            for (int i = 0; i < maxInRow.Count(); i++)
+            {
+                Console.Write(maxInRow[i]+", ");
+            }
+            Console.WriteLine();
+            Console.Write("Мінімуми рядків: ");
+            for (int i = 0; i < minInRow.Count(); i++)
+            {
+                Console.Write(minInRow[i] + ", ");
+            }
+            Console.WriteLine();
+
+            double[] matem = new double[3];
+
+            Console.Write("Значеняя з формули - [k*min + (1-k)*max]: ");
+            for(int i = 0; i < matem.Count(); i++)
+            {
+                matem[i] = (coeff * minInRow[i]) + ((1 - coeff) * maxInRow[i]);
+                Console.Write(matem[i] + ", ");
+            }
+            Console.WriteLine();
+
+            int bestRow = 0;
+
+            for (int i = 0; i < matem.Count(); i++)
+            {
+                if (matem.Max() == matem[i])
+                {
+                    bestRow = i;
+                    Console.WriteLine("Найбільше з цих значень: " + matem.Max() + "  У рядку: " + (bestRow + 1));
+                }
+            }
+
+            Console.Write("Найкраще рішення: ");
+            for (int j = 0; j < data.GetLength(1); j++)
+            {
+                Console.Write(data[bestRow, j] + ", ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine();
+        }
+
+        public static void CriterionBayesLaplace(int[,] data)
+        {
+            Console.WriteLine();
+            Console.WriteLine("     Критерій Байєса-Лапласа");
+
+            double[] matem = new double[3];
+            double[] coeff = new double[3];
+            coeff[0] = 0.5;
+            coeff[1] = 0.35;
+            coeff[2] = 0.15;
+            Console.Write("Значеняя з формули - [A1*k1 + A2*k2 + A3*k3]: ");
+
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                matem[i] = data[i, 0] * coeff[0] + data[i, 1] * coeff[1] + data[i, 2] * coeff[2];
+                Console.Write(matem[i] + ", ");
+            }
+            Console.WriteLine();
+
+            int bestRow = 0;
+
+            for (int i = 0; i < matem.Count(); i++)
+            {
+                if (matem.Max() == matem[i])
+                {
+                    bestRow = i;
+                    Console.WriteLine("Найбільше з цих значень: " + matem.Max() + "  У рядку: " + (bestRow + 1));
+                }
+            }
+
+            Console.Write("Найкраще рішення: ");
+            for (int j = 0; j < data.GetLength(1); j++)
+            {
+                Console.Write(data[bestRow, j] + ", ");
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            var data = ReadData();
+
+            PrintData(data); // Вивід масиву на екран
+
+            Algoritms.CriterionWald(data);
+            Algoritms.СriterionLaplace(data);
+            Algoritms.CriterionHurwitz(data);
+
+            Algoritms.CriterionBayesLaplace(data);
+
+            Console.ReadLine();
+        }
+
+        public static int[,] ReadData()
+        {
+            string[] lines = File.ReadAllLines(@"C:\Users\Petro\Desktop\Algoritm\TPR\Lab_1\data.txt");
+            int[,] num = new int[lines.Length, lines[0].Split(' ').Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string[] temp = lines[i].Split(' ');
+                for (int j = 0; j < temp.Length; j++)
+                    num[i, j] = Convert.ToInt32(temp[j]);
+            }
+            // проверяем выводом на консоль
+            return num;
+        }
+
+        public static void PrintData(int[,] data)
+        {
+            Console.WriteLine("   Вхідні дані:");
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                for (int j = 0; j < data.GetLength(1); j++)
+                {
+                    Console.Write(data[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
